b88ba6f35bf93a34703adb5de26a3b3b92728e68
Sascha Ißbrücker <sissbruecker@vaadin.com>
2022-12-15 11:52:10 +0100

    Extend dialog example

--- src/app/examples/newsletter-dialog/newsletter-dialog.component.ts
+++ src/app/examples/newsletter-dialog/newsletter-dialog.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
 import { Notification } from '@vaadin/notification';
+import '@vaadin/checkbox';
 import '@vaadin/dialog';
 import '@vaadin/horizontal-layout';
 import '@vaadin/text-field';
+
+interface NewsletterOptions {
+  marketing: boolean;
+  releases: boolean;
+  blog: boolean;
+}
 
 @Component({
   selector: 'app-newsletter-dialog',
@@ -11,18 +18,34 @@ import '@vaadin/text-field';
 })
 export class NewsletterDialogComponent {
   dialogOpened: boolean = false;
-  newsletterMail: string = '';
+  mail: string = '';
+  showOptions: boolean = false;
+  options: NewsletterOptions = {
+    marketing: true,
+    releases: true,
+    blog: true,
+  };
 
   openDialog() {
     this.dialogOpened = true;
   }
 
   signup() {
-    Notification.show(`${this.newsletterMail} signed up for newsletter`, {
-      theme: 'primary',
-    });
+    Notification.show(
+      `${this.mail} signed up for newsletter ${JSON.stringify(this.options)}`,
+      {
+        theme: 'primary',
+      }
+    );
+    // Reset dialog
     this.dialogOpened = false;
-    this.newsletterMail = '';
+    this.mail = '';
+    this.showOptions = false;
+    this.options = {
+      marketing: true,
+      releases: true,
+      blog: true,
+    };
   }
 
   cancel() {
